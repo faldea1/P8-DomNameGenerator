bf36cf2ac4b9a670e50f0f36ee9d944b04cabf6d
faldea1 <faldea1@uc.cl>
2022-12-04 03:33:51 +0000

    arraywithindexpositions

--- app.js
+++ app.js
@@ -13,4 +13,18 @@
 
 //console.log(ArraySimulation);
 
+let pronoun = ['the', 'our', 'that'];
+let adj = ['great', 'big', 'nice'];
+let noun = ['jogger', 'racoon', 'bee'];
+let extension =['.com', '.cl', '.net', '.us', '.ar', '.io']
+
+for (let i=0; i<pronoun.length; i++){
+    for (let j=0; j<adj.length; j++){
+        for (let k=0; k<noun.length; k++){
+            for (let l=0; l<extension.length; l++){
+                console.log(`${i}${j}${k}${l}`);
+            }
+        }
+    }
+}
 
