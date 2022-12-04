2175b524220af44577da1de85d3aa04c60e3906b
faldea1 <faldea1@uc.cl>
2022-12-04 02:58:33 +0000

    ejemplo de array

---
+++ app.js
@@ -0,0 +1,16 @@
+//DOMAIN NAME GENERATOR//
+
+//let ArraySimulation = [];
+
+//for (a=1; a<20; a++){
+
+  //  for (b=1; b<20; b++){
+    //    ArraySimulation.push(`${a} * ${b} = ${a*b}`)
+
+//    }
+
+//}
+
+//console.log(ArraySimulation);
+
+
